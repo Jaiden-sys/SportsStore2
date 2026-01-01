dcffdbf383fbe2b1275322e4bcf4fe2c1b3fa659
Roman Eliseev <eleseev2479@gmail.com>
2026-01-01 20:41:58 +0300

    Prefixed values in pagelinktaghelper

--- Program.cs
+++ Program.cs
@@ -9,14 +9,20 @@ builder.Services.AddDbContext<StoreDbContext>(opts => { opts.UseSqlServer(builde
 });
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 var app = builder.Build();
-
+    
 
 app.UseStaticFiles();
 
-app.MapControllerRoute("pagination",
-    "Products/Page{productPage}",
+app.MapControllerRoute("catpage",
+"{category}/Page{productPage:int}",
     new { Controller = "Home", action = "Index" });
-app.MapDefaultControllerRoute();
+app.MapControllerRoute("page", "Page{productPage:int}",
+    new { Controller = "Home", action = "Index", productPage = 1 });
+app.MapControllerRoute("category", "{category}",
+    new { Controller = "Home", action = "Index", productPage = 1 });
+app.MapControllerRoute("pagination",
+"Products/Page{productPage}",
+    new { Controller = "Home", action = "Index", productPage = 1 });
 
 SeedData.EnsurePopulated(app);
 app.UseHttpLogging();
