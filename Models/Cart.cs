c397b21dfe00a06bebcd1bfb5ff818fb7777a4b9
Roman Eliseev <eleseev2479@gmail.com>
2026-01-09 21:24:04 +0300

    Creating Cart service. NOT actual DI

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -6,14 +6,14 @@ namespace SportsStore2.Models
     public class CartLine
     {
         public int CartLineId { get; set; }
-        public Product Product { get; set; } = new Product();
+        public Product Product { get; set; } = new();
         public int Quantity { get; set; }
     }
     public class Cart
     {
         public List<CartLine> Lines { get; set; } = new List<CartLine>();
 
-        public void AddItem(Product product, int quantity)
+        public virtual void AddItem(Product product, int quantity)
         {
             CartLine? line = Lines
                 .Where(p => p.Product.ProductID == product.ProductID)
@@ -30,11 +30,11 @@ namespace SportsStore2.Models
                 line.Quantity += quantity;
             }
         }
-        public void RemoveLine (Product product) => 
+        public virtual void RemoveLine (Product product) => 
             Lines.RemoveAll(l => l.Product.ProductID == product.ProductID);
            
-        public decimal ComputeTotalValue() =>
+        public virtual decimal ComputeTotalValue() =>
             Lines.Sum(e => e.Product.Price * e.Quantity);
-        public void Clear() => Lines.Clear();
+        public virtual void Clear() => Lines.Clear();
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<StoreDbContext>(opts => { opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]); 
 });
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
-
+builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddRazorPages();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
