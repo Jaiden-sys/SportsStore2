6b90d94187fc1edad60a36b10b6d0fa60b198be3
Roman Eliseev <eleseev2479@gmail.com>
2026-01-11 16:20:18 +0300

    Added Cart, started Order proceeding

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore2.Models;
+
+namespace SportsStore2.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
