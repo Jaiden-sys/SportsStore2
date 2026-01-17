21c18090040881a7220554576b9ffd22d5e310e2
Roman Eliseev <eleseev2479@gmail.com>
2026-01-18 02:09:47 +0300

    Administration is done with error handling

--- Program.cs
+++ Program.cs
@@ -21,6 +21,15 @@ builder.Services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlSer
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
 
 var app = builder.Build();
+if (app.Environment.IsProduction())
+{
+    app.UseExceptionHandler("/error");
+}
+app.UseRequestLocalization(opts => {
+    opts.AddSupportedCultures("en-US")
+    .AddSupportedUICultures("en-US")
+    .SetDefaultCulture("en-US");
+});
 
 app.UseAuthentication();
 app.UseAuthorization();
